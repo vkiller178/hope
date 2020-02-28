5ae9a85f775feddfe014446b341f513c87570120
阮细浩 <rxh12352@gmail.com>
2020-02-28 22:04:35 +0800

    remove js file

--- src/db/models/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var user_1 = require("./user");
-exports.UserModel = user_1["default"];
-var post_1 = require("./post");
-exports.PostModel = post_1["default"];
