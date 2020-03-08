45010658a8e2f247f4357e6fb3eed934f1c07116
阮细浩 <rxh12352@gmail.com>
2020-03-08 18:57:58 +0800

    fix: 首页无限加载优化；VirtualScroll组件

---
+++ src/nextRoot/components/VirtualScroll/Index.tsx
@@ -0,0 +1,253 @@
+import React, { useEffect, useState, useRef } from 'react'
+
+// 获取数据的间隔
+const scrollTake = 400
+// 容器触底的阈值
+const delta = 10
+// 超出可视区域后隐藏的阈值
+const outViewDelta = 500
+
+enum ScrollDirection {
+  up = 'up, view more',
+  down = 'down, view history',
+}
+
+export enum LoadState {
+  loading = 'loading',
+  noMore = 'noMore',
+  needMore = 'neewMore',
+}
+
+interface ScrollItem {
+  dom: {
+    offsetTop: number
+  }
+
+  [key: string]: any
+}
+interface VirtualScrollProps {
+  indicator?: React.ComponentType<{ load: LoadState }>
+  scrollBinderEleQuery?: string
+  renderItem: (item: ScrollItem) => any
+  key?: string
+  fetchMethod: (items: Array<ScrollItem>) => Promise<Array<ScrollItem>>
+  component?: string | any
+}
+
+const VirtualScroll: React.FC<VirtualScrollProps> = ({
+  children,
+  indicator: Indicator,
+  scrollBinderEleQuery,
+  renderItem,
+  key,
+  fetchMethod,
+  component: Component,
+}) => {
+  const [scrollItemsStore, setScrollItemsStore] = useState<Array<ScrollItem>>(
+    []
+  )
+  const [dScrollItemsStore, setDScrollItemsStore] = useState<Array<ScrollItem>>(
+    []
+  )
+
+  const id = useRef<string>('')
+  const lastTick = useRef<number>(0)
+
+  const [load, setLoad] = useState<{ loading: LoadState }>({
+    loading: LoadState.needMore,
+  })
+  const wrapperCls = useRef<any>()
+  wrapperCls.current = () => `virtual-scroll-wrapper-${id.current}`
+  const itemCls = 'scroll-item'
+
+  key = key || 'id'
+
+  const getFeed = useRef<any>()
+  getFeed.current = async () => {
+    setLoad(_ => ({ ..._, loading: LoadState.loading }))
+    const feeds = await fetchMethod(scrollItemsStore)
+
+    lastTick.current = performance.now()
+
+    if (feeds.length > 0) {
+      setScrollItemsStore(_ => _.concat(feeds))
+      setDScrollItemsStore(_ => _.concat(feeds))
+      setLoad(_ => ({ ..._, loading: LoadState.needMore }))
+    } else {
+      setLoad(_ => ({ ..._, loading: LoadState.noMore }))
+    }
+  }
+  useEffect(() => {
+    let lastScrollTop = 0
+
+    const scrollBinderEle = document.querySelector(
+      scrollBinderEleQuery || 'body'
+    )
+
+    id.current = (Math.random() * Math.pow(10, 18)).toFixed(0)
+
+    function handler(e: any) {
+      const curScrollTop = e.currentTarget.scrollTop
+
+      const postsWrapper = document.querySelector<HTMLElement>(
+        `.${wrapperCls.current()}`
+      )
+      const postElements = document.querySelectorAll<HTMLElement>(
+        `.${wrapperCls.current()} .${itemCls}`
+      )
+
+      postsWrapper.style.position = 'relative'
+
+      let direction: ScrollDirection
+      if (curScrollTop - lastScrollTop > 0) {
+        direction = ScrollDirection.up
+      } else {
+        direction = ScrollDirection.down
+      }
+
+      console.log(direction, e.currentTarget)
+
+      lastScrollTop = curScrollTop
+
+      const isOutBody = (ele: HTMLElement) => {
+        const { top, height } = ele.getBoundingClientRect()
+        let scope = [
+          0 - height - outViewDelta,
+          scrollBinderEle.clientHeight + outViewDelta,
+        ]
+        return (
+          Math.min(...scope, top) === top || Math.max(...scope, top) === top
+        )
+      }
+
+      Array.from(postElements).forEach((postE, index) => {
+        // add scrolTop info, if not yet
+        setScrollItemsStore(posts => {
+          const index = posts.findIndex(
+            p => p.id === +postE.getAttribute('data-item-key')
+          )
+          posts[index] = {
+            ...posts[index],
+            dom: {
+              offsetTop: postE.offsetTop,
+            },
+          }
+          return posts
+        })
+        if (direction === ScrollDirection.up) {
+          const isBeforeDividE =
+            index < postElements.length - 1 &&
+            isOutBody(postE) &&
+            !isOutBody(postElements[index + 1])
+          if (isBeforeDividE) {
+            const topIndex = index + 1
+            postsWrapper.style.paddingTop =
+              postElements[topIndex].offsetTop + 'px'
+            setDScrollItemsStore(_ => _.slice(topIndex))
+          }
+        } else {
+          const isLastDividE =
+            index > 0 && !isOutBody(postElements[index - 1]) && isOutBody(postE)
+
+          if (isLastDividE) {
+            setDScrollItemsStore(_ => _.slice(0, index - 1))
+          }
+        }
+      })
+
+      const isDown = direction === ScrollDirection.down
+      let compition = {
+        index: isDown ? 0 : postElements.length - 1,
+        compare() {
+          return isDown
+            ? postElements[this.index].offsetTop > lastScrollTop - outViewDelta
+            : postElements[this.index].getBoundingClientRect().y <
+                scrollBinderEle.clientHeight + outViewDelta
+        },
+        hasinsertE(posts: Array<ScrollItem>) {
+          const _index = posts.findIndex(
+            p =>
+              p.id === +postElements[this.index].getAttribute('data-item-key')
+          )
+
+          return (dPosts: Array<ScrollItem>) => {
+            if (isDown && _index > 0) {
+              postsWrapper.style.paddingTop =
+                posts[_index - 1].dom.offsetTop + 'px'
+
+              dPosts.unshift(posts[_index - 1])
+            } else if (!isDown && _index < posts.length - 1) {
+              dPosts.push(posts[_index + 1])
+            }
+
+            return [...dPosts]
+          }
+        },
+      }
+
+      insertBack()
+
+      function insertBack() {
+        if (compition.compare) {
+          let last: number = 0
+          setScrollItemsStore(_ => {
+            const now = performance.now()
+            if (now - last > 200) {
+              last = now
+              const dPostInsert = compition.hasinsertE(_)
+              setDScrollItemsStore(dp => dPostInsert(dp))
+            }
+
+            return _
+          })
+        }
+      }
+
+      const now = performance.now()
+      const { bottom } = postsWrapper.getBoundingClientRect()
+      if (
+        load.loading !== LoadState.loading &&
+        Math.abs(bottom - scrollBinderEle.clientHeight) < delta &&
+        now - lastTick.current > scrollTake &&
+        direction === ScrollDirection.up
+      ) {
+        // 触底
+        getFeed.current()
+      }
+    }
+
+    getFeed.current()
+
+    console.log(scrollBinderEle)
+
+    scrollBinderEle.addEventListener('scroll', handler)
+
+    return () => {
+      scrollBinderEle.removeEventListener('scroll', handler)
+    }
+  }, [])
+
+  if (Component) {
+    return (
+      <Component className={wrapperCls.current()}>
+        {dScrollItemsStore.map(item => (
+          <div className={itemCls} key={item[key]} data-item-key={item[key]}>
+            {renderItem(item)}
+          </div>
+        ))}
+        {Indicator && <Indicator load={load.loading} />}
+      </Component>
+    )
+  }
+  return (
+    <div className={wrapperCls.current()}>
+      {dScrollItemsStore.map(item => (
+        <div className={itemCls} key={item[key]} data-item-key={item[key]}>
+          {renderItem(item)}
+        </div>
+      ))}
+      {Indicator && <Indicator load={load.loading} />}
+    </div>
+  )
+}
+export default VirtualScroll
--- src/nextRoot/pages/index.tsx
+++ src/nextRoot/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, DOMElement } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import { get, post } from '../js/request'
 import {
@@ -7,17 +7,16 @@ import {
   Typography,
   Box,
   Card,
-  IconButton,
   Button,
   makeStyles,
   Hidden,
   Link,
 } from '@material-ui/core'
-import { useRouter } from 'next/router'
 import { ThumbUp, Visibility } from '@material-ui/icons'
-import { useWindowSize } from 'react-use'
 
 import Skeleton from '@material-ui/lab/Skeleton'
+import clsx from 'clsx'
+import VirtualScroll, { LoadState } from '../components/VirtualScroll/Index'
 
 interface Post {
   id: number
@@ -30,15 +29,17 @@ interface Post {
   like: number
   click: number
   brief: string
+  dom: {
+    offsetTop: number
+  }
 }
 
 const useCardStyle = makeStyles(theme => ({
   root: {
     backgroundColor: '#fff',
-    '&:not(:last-child)': {
-      borderBottom: '1px solid rgb(240,242,247)',
-    },
+    borderBottom: '1px solid rgb(240,242,247)',
     padding: theme.spacing(2),
+    minHeight: 100,
   },
   p: {
     marginBottom: theme.spacing(2),
@@ -48,8 +49,33 @@ const useCardStyle = makeStyles(theme => ({
   },
 }))
 
+const Indicator: React.FC<{ load: LoadState }> = ({ load }) => {
+  const classesCard = useCardStyle()
+
+  return (
+    <Grid
+      container
+      direction="column"
+      alignItems="stretch"
+      justify="center"
+      className={classesCard.root}
+    >
+      {load === LoadState.loading ? (
+        <>
+          <Skeleton variant="text" width={60} />
+          <Skeleton variant="text" width={160} />
+          <Skeleton variant="text" />
+        </>
+      ) : load === LoadState.noMore ? (
+        <Grid container justify="center" alignItems="center">
+          <span>暂无更多</span>
+        </Grid>
+      ) : null}
+    </Grid>
+  )
+}
+
 const PostCard: React.FC<Post> = p => {
-  const router = useRouter()
   const classes = useCardStyle()
 
   function viewPage() {
@@ -112,10 +138,17 @@ const useIndexStyle = makeStyles(theme => ({
   },
   content: {
     // backgroundColor: '#fff',
+    position: 'relative',
   },
-  root: {
-    height: '667px',
-    overflowY: 'scroll',
+  root: {},
+  '@global': {
+    'html,body,#__next': {
+      height: '100%',
+    },
+    '#__next': {
+      overflowY: 'scroll',
+      overscrollBehaviorY: 'none',
+    },
   },
 }))
 
@@ -130,88 +163,38 @@ export function useMixedWindowSize() {
 }
 
 const Index: React.FC = () => {
-  const [posts, setPosts] = useState<Array<Post>>([])
   const classes = useIndexStyle()
-  const classesCard = useCardStyle()
 
-  const { height } = useMixedWindowSize()
-
-  const [load, setLoad] = useState<{ loading: boolean }>({
-    loading: false,
-  })
-
-  const [lastTick, setLastTick] = useState(0)
-
-  const getFeed = async () => {
-    setLoad(_ => ({ loading: true }))
-
+  const getFeed = async posts => {
     const feeds = await get<Array<Post>>('/open/feed', {
       skip: posts.length,
       take: 10,
     })
 
-    setLastTick(performance.now())
-
-    if (feeds.length > 0) {
-      setPosts(_ => _.concat(feeds))
-    }
-    setLoad(_ => ({ ..._, loading: false }))
-  }
-
-  useEffect(() => {
-    getFeed()
-  }, [])
-
-  const scrollWrapper = useRef<HTMLElement>()
-
-  const scrollTake = 400
-
-  function onScroll() {
-    const now = performance.now()
-    if (
-      scrollWrapper.current &&
-      scrollWrapper.current.scrollTop + height ===
-        scrollWrapper.current.scrollHeight &&
-      load.loading === false &&
-      now - lastTick > scrollTake
-    ) {
-      getFeed()
-    }
+    return feeds
   }
 
   return (
-    <div
-      ref={ref => (scrollWrapper.current = ref)}
-      className={classes.root}
-      style={{ height }}
-      onScroll={onScroll}
-    >
+    <div className={classes.root}>
       <Header />
-      <div>
-        <Container style={{ marginTop: '16px' }} maxWidth="md">
-          <Grid spacing={2} container alignItems="flex-start">
-            <Grid className={classes.content} item lg={9} xs={12} sm={12}>
-              {posts.map(p => (
-                <PostCard {...p} key={p.id} />
-              ))}
-
-              {load.loading && (
-                <div className={classesCard.root}>
-                  <Skeleton variant="text" width={60} />
-                  <Skeleton variant="text" width={160} />
-                  <Skeleton variant="text" />
-                </div>
-              )}
-            </Grid>
-
-            <Hidden mdDown>
-              <Grid className={classes.sidebar} item sm={3}>
-                <Card className="cardInfo">222</Card>
-              </Grid>
-            </Hidden>
+      <Container style={{ marginTop: '16px' }} maxWidth="md">
+        <Grid spacing={2} container alignItems="flex-start">
+          <Grid className={clsx(classes.content)} item lg={9} xs={12} sm={12}>
+            <VirtualScroll
+              fetchMethod={posts => getFeed(posts)}
+              renderItem={(item: any) => <PostCard {...item} />}
+              indicator={Indicator}
+              scrollBinderEleQuery="#__next"
+            />
           </Grid>
-        </Container>
-      </div>
+
+          <Hidden mdDown>
+            <Grid className={classes.sidebar} item sm={3}>
+              <Card className="cardInfo">222</Card>
+            </Grid>
+          </Hidden>
+        </Grid>
+      </Container>
     </div>
   )
 }
