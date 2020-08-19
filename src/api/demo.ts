4e7f7f73a852b7f0982d641484ddb3fbf3624f5f
阮细浩 <ruanxihao@fcbox.com>
2020-08-19 15:23:36 +0800

    fix: 增加api示例

---
+++ src/api/demo.ts
@@ -0,0 +1,24 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Ctx,
+  CookieParam,
+  CookieParams,
+  Params,
+  BodyParam,
+} from 'routing-controllers'
+import { UserModel } from '../db/models'
+import { bundleWithCode } from '../utils/errorbundle'
+import { createToken, decodeToken } from '../middlewares/jwt'
+import { Context } from 'koa'
+
+@Controller('/demo')
+export default class User {
+  @Get('/')
+  async allUser(@Param('id') id) {
+    return 'hello world'
+  }
+}
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export { default as user } from './user'
 export { default as post } from './post'
 export { default as userAction } from './userActions'
+export { default as demo } from './demo'
